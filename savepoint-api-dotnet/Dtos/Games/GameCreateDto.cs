ab6abf84abe4bcaba43f76163aa526322318e0f7
Taha Rashid <taharashid710192@gmail.com>
2025-08-22 16:00:35 -0400

    feat: created GameUpdate, GameCreate Dtos
    - Moved game dtos to /games folder within dtos folder

--- savepoint-api-dotnet/Dtos/Games/GameCreateDto.cs
+++ savepoint-api-dotnet/Dtos/Games/GameCreateDto.cs
@@ -1,14 +1,16 @@
-﻿namespace savepoint_api_dotnet.Dtos
+﻿using savepoint_api_dotnet.Models;
+
+namespace savepoint_api_dotnet.Dtos.Games
 {
-    public class GameDto
+    public class GameCreateDto
     {
         public Guid Id { get; set; }
         public DateTime Dtc { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Cover { get; set; }
-        public List<DeveloperDto> Developers { get; set; }
-        public List<GenreDto> Genres { get; set; }
+        public List<int> DeveloperIds { get; set; }
+        public List<int> GenreIds { get; set; }
         public List<ImageDto> Images { get; set; }
         public List<VideoDto> Videos { get; set; }
     }
---
+++ savepoint-api-dotnet/Dtos/Games/GameUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using savepoint_api_dotnet.Models;
+
+namespace savepoint_api_dotnet.Dtos.Games
+{
+    public class GameUpdateDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Dtc { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Cover { get; set; }
+        public List<int> DeveloperIds { get; set; }
+        public List<int> GenreIds { get; set; }
+        public List<ImageDto> Images { get; set; }
+        public List<VideoDto> Videos { get; set; }
+    }
+}
