aa1a88a786f2ad82b060f41bc8251da657a497e7
Taha Rashid <taharashid710192@gmail.com>
2025-08-31 15:38:21 -0400

    feat: created SearchController with search endpoints
    - Can search for name, description, genre, and developer

---
+++ savepoint-api-dotnet/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using savepoint_api_dotnet.Models;
+using savepoint_api_dotnet.Services;
+using savepoint_api_dotnet.Dtos.Games;
+
+namespace savepoint_api_dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly GameService _gameService;
+
+        // Dependency injection
+        public SearchController(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        /// <summary>
+        /// Returns search results for games
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        public IActionResult SearchGames([FromQuery] string searchTerm)
+        {
+            var gameDtos = _gameService.GetGames()
+                .Where(g => g.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            g.Description.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            g.Developers.Any(d => d.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)) ||
+                            g.Genres.Any(ge => ge.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)))
+                .ToList();
+            return Ok(gameDtos);
+        }
+    }
+}
